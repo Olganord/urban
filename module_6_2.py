0897851d6dacd1ab3a4385c5f487e55741100ce8
Olganord <156664559+Olganord@users.noreply.github.com>
2024-09-30 11:36:04 +0500

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -73,51 +73,51 @@ vehicle1.print_info()
 
 
 class Vehicle:
-    __COLOR_VARIANTS = ['yellow', 'purple', 'black']
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, _model, _engine_power, _color):
-        self.owner = owner
-        self._model = _model
-        self._engine_power = _engine_power
-        self._color = _color
+    def __init__(self, owner, model, color, engine_power):
+        self.owner = owner       # Владелец транспорта
+        self.__model = model     # Модель транспорта
+        self.__engine_power = engine_power  # Мощность двигателя
+        self.__color = color     # Цвет транспорта
 
     def get_model(self):
-        return f'Модель: {self._model}'
+        return f"Модель: {self.__model}"
 
     def get_horsepower(self):
-        return f'Мощность двигателя: {self._engine_power}'
+        return f"Мощность двигателя: {self.__engine_power}"
 
     def get_color(self):
-        return f'Цвет: {self._color}'
-
-    def set_color(self, new_color):
-        if new_color.lower() in self.__COLOR_VARIANTS:
-            self._color = new_color
-        else:
-            print('Нельзя сменить цвет на ' + f'{new_color}')
+        return f"Цвет: {self.__color}"
 
     def print_info(self):
         print(self.get_model())
         print(self.get_horsepower())
         print(self.get_color())
-        print(f'Владелец: {self.owner}')
+        print(f"Владелец: {self.owner}")
 
+    def set_color(self, new_color):
+        if new_color.lower() in map(str.lower, self.__COLOR_VARIANTS):
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
 
 class Sedan(Vehicle):
-    # __PASSENGERS_LIMIT = 5 # атрибут не используется
-        pass
+    __PASSENGERS_LIMIT = 5  # Лимит пассажиров
 
-
-# Текущие цвета __COLOR_VARIANTS = ['yellow', 'purple', 'black']
-vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, 'blue')
+# Создаем объект класса Sedan
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 
 # Изначальные свойства
 vehicle1.print_info()
 
-# Меняем свойства (в т.ч., вызывая методы)
+# Меняем свойства
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
 
 # Проверяем что поменялось
 vehicle1.print_info()
+
+
+
